e3a571eaf2d3501872c37839c0c4753e4297be1c
manutero <manutero@gmail.com>
2015-07-03 13:24:51 +0200

    Working on post & put

--- src/controllers/airport_controller.ts
+++ src/controllers/airport_controller.ts
@@ -7,30 +7,82 @@ import express = require("express");
 import mongoose = require("mongoose");
 
 import airportModule = require("../models/airport");
+import AirportProperties = airportModule.AirportProperties;
 import AirportInterface = airportModule.AirportInterface;
 import Airport = airportModule.AirportModel;
 
 export var router = express.Router();
 router.get('/:key', findByKey);
 router.post('/', create);
+router.patch('/:key', update);
+router.put('/:key', overwrite);
 
+
+/* private functions */
+
+/**
+ * POST /airport
+ */
 function create(request: express.Request, res: express.Response) {
-    var data = {
-        key: request.body.key,
-        name: request.body.name,
-        lat: request.params.lat,
-        lon: request.params.lon
-    };
+    var data = getData(request);
     var onCreate = function(error: any, obj: AirportInterface) {
-        error ? res.send(400) : res.send("Airport created: " + obj.toString());
+        error ? res.sendStatus(400) : res.send("Airport created: " + obj.toString());
     };
     Airport.create(data, onCreate);
 }
 
+/**
+ * GET /airport/{key}
+ */
 function findByKey(request: express.Request, res: express.Response) {
-    var data = { key: request.params.key };
+    var query = { key: request.params.key };
     var onFind = function(error: any, obj: AirportInterface) {
-        error ? res.send(400) : res.json(obj);
+        error ? res.sendStatus(400) : res.json(obj);
     }
-    Airport.findOne(data, onFind);
+    Airport.findOne(query, onFind);
+}
+
+/**
+ * PATCH /airport/{key}
+ */
+function update(request: express.Request, res: express.Response) {
+    var data = getData(request);
+    console.log("DATA: " + JSON.stringify(data));
+    data.key = request.params.key;
+    var query = { key: data.key };
+    var onFind = function(error: any, obj: AirportInterface) {
+        error ? res.sendStatus(400) : _update(query, obj, res); // FIXME obj
+    }
+    Airport.findOne(query, onFind);
+}
+
+function _update(query: Object, obj: AirportInterface, res: express.Response) {
+    var onUpdate = function(error: any, raw: any) {
+        error ? res.sendStatus(400) : res.send("Updated: " + obj.toString());
+    };
+    Airport.update(query, obj, onUpdate);
+}
+
+/**
+ * PUT /airport/{key}
+ */
+function overwrite(request: express.Request, res: express.Response) {
+    var data = getData(request);
+    data.key = request.params.key;
+    var query = { key: data.key };
+    console.log(JSON.stringify(data));
+    var onUpdate = function(error: any, raw: any) {
+        console.log(error.toString());
+        error ? res.sendStatus(400) : res.send("Updated: " + raw);
+    };
+    Airport.update(query, data, onUpdate);
+}
+
+function getData(request: express.Request) : AirportProperties {
+   return {
+        key: request.body.key,
+        name: request.body.name,
+        lat: request.body.lat,
+        lon: request.params.lon,
+    };
 }
--- src/models/airport.ts
+++ src/models/airport.ts
@@ -1,25 +1,46 @@
 /*
- * Exports the AirportInterface and the AirportModel
+ * Exports the AirportProperties, AirportInterface and AirportModel
  */
-
 
 import mongoose = require("mongoose");
 
-export interface AirportInterface extends mongoose.Document {
+export interface AirportProperties {
     key:  string;
-    name: string;
-    lat:  number;
-    lon:  number;
-    connections() : AirportInterface[];
+    name?: string;
+    lat?:  number;
+    lon?:  number;
+}
+
+export interface AirportInterface extends AirportProperties, mongoose.Document {
+    created_at: Date;
+    updated_at: Date;
+    connections(): AirportInterface[];
 }
 
 var airportSchema = new mongoose.Schema({
-    key:  {type: String, required: true},
+    key:  {type: String, required: true, uppercase:true, unique:true},
     name: {type: String},
-    lat:  {type: Number},
-    lon:  {type:Number}
+    lat:  {type: Number, default: null},
+    lon:  {type:Number,  default: null},
+    created_at: {type: Date},
+    updated_at: {type: Date},
 });
 
+/**
+ * created_at & updated_at auto value
+ */
+airportSchema.pre('save', function(next) {
+  var now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+
+/**
+ * @override
+ */
 airportSchema.method("toString()", function(){
     return JSON.stringify(this);
 });
@@ -28,5 +49,6 @@ airportSchema.method("connections", function() {
     // TODO
     return [];
 });
+
 
 export var AirportModel = mongoose.model<AirportInterface>("Airport", airportSchema);
